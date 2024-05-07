76e40a09dc86c85d8d7dc6b369eb857c270d64ab
Akhils-lanatus <akhil.s@lanatussystems.com>
2024-05-07 12:15:10 +0530

    NST and ST

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  let teams = ["MI", "CSK", "RCB", "LSG", "DC", "KXIP", "SRH", "KKR"];
-  let days = [
+  let [teams] = useState([
+    "MI",
+    "CSK",
+    "RCB",
+    "LSG",
+    "DC",
+    "KXIP",
+    "SRH",
+    "KKR",
+  ]);
+  let [days] = useState([
     "MONDAY",
     "TUESDAY",
     "WEDNESDAY",
@@ -10,35 +19,75 @@ const App = () => {
     "FRIDAY",
     "SATURDAY",
     "SUNDAY",
-  ];
-  const matchSchedule = [];
+  ]);
+
   let totalMatches = teams.length * (teams.length - 1);
+  const matchSchedule = [];
 
   let counterWatcher = teams.map((items) => {
     return { team: items, teamMatchCount: 0 };
   });
 
-  const mindex = [];
   const selectedMatches = [];
+  let previousMatchChecker = [];
+  let testCounter = 0;
 
-  const getMatchDayIndex = () => {
-    let num = Math.floor(Math.random() * totalMatches);
-    const isExist = mindex.includes(num);
-    if (isExist) {
-      return getMatchDayIndex();
+  const nonShuffledTeams = [];
+  for (let i = 0; i < teams.length; i++) {
+    for (let j = 0; j < teams.length; j++) {
+      if (i !== j) {
+        nonShuffledTeams.push({ team1: teams[i], team2: teams[j] });
+      }
+    }
+  }
+
+  const getRandomMatches = () => {
+    let team1 = Math.floor(Math.random() * teams.length);
+    let team2 = Math.floor(Math.random() * teams.length);
+    if (team1 !== team2) {
+      previousMatchChecker.push({ team1, team2 });
+      if (previousMatchChecker.length == 1) {
+        ++counterWatcher[team1].teamMatchCount;
+        ++counterWatcher[team2].teamMatchCount;
+        return { team1, team2 };
+      } else if (previousMatchChecker.length > 1) {
+        if (
+          previousMatchChecker[previousMatchChecker.length - 2].team1 !=
+            team1 &&
+          previousMatchChecker[previousMatchChecker.length - 2].team2 !=
+            team1 &&
+          previousMatchChecker[previousMatchChecker.length - 2].team1 !=
+            team2 &&
+          previousMatchChecker[previousMatchChecker.length - 2].team2 != team2
+        ) {
+          counterWatcher[team1].teamMatchCount = ++counterWatcher[team1]
+            .teamMatchCount;
+          counterWatcher[team2].teamMatchCount = ++counterWatcher[team2]
+            .teamMatchCount;
+          return { team1, team2 };
+        } else {
+          previousMatchChecker.pop();
+          return getRandomMatches();
+        }
+      }
     } else {
-      mindex.push(num);
-      return num;
+      return getRandomMatches();
     }
   };
 
   for (let i = 0; i < totalMatches; i++) {
-    let matchPosition = getMatchDayIndex();
     let dayIndex = matchSchedule.length % 7;
-    matchSchedule.push({ day: days[dayIndex], matchPosition });
+    let matchToBePlayed = getRandomMatches();
+    matchSchedule.push({
+      day: days[dayIndex],
+      match: `${teams[matchToBePlayed.team1]} v/s ${
+        teams[matchToBePlayed.team2]
+      }`,
+    });
   }
 
-  return <div>App</div>;
+  //consoles
+  console.log(nonShuffledTeams, "NST");
+  console.log(matchSchedule, "ST");
 };
-
 export default App;
